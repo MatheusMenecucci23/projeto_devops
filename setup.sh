9d6a9f3741d63a7d99b0e7cb041c7f629f19ef7c
matheusmenecucci <menecucci40@gmail.com>
2025-07-17 17:56:40 -0300

    .

--- setup.sh
+++ setup.sh
@@ -5,10 +5,6 @@ echo "Verificando a branch do Git..."
 git branch
 git checkout helm
 
-# Iniciar o Minikube
-echo "Iniciando o Minikube..."
-minikube start
-
 # Ativar o addon de Ingress no Minikube
 echo "Ativando o addon de Ingress no Minikube..."
 minikube addons enable ingress
@@ -16,11 +12,6 @@ minikube addons enable ingress
 # Abrir o dashboard do Minikube
 echo "Abrindo o dashboard do Minikube..."
 minikube dashboard &
-
-# Modificar o arquivo /etc/hosts
-echo "Modificando o arquivo /etc/hosts..."
-sudo bash -c 'echo "192.168.49.2  k8s.local" >> /etc/hosts'
-sudo bash -c 'echo "192.168.49.2  backend.k8s.local" >> /etc/hosts'
 
 # Rodar a aplicação com Helm
 echo "Rodando a aplicação com Helm..."
