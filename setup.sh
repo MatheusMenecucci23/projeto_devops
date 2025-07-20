fd298c62a786255bee6e50e55bc1dda4bcff0ddb
matheusmenecucci <menecucci40@gmail.com>
2025-07-20 10:25:46 -0300

    setup.sh

--- setup.sh
+++ setup.sh
@@ -9,16 +9,15 @@ git checkout helm
 
 # Ativar o addon de Ingress no Minikube
 echo "Ativando o addon de Ingress no Minikube..."
-minikube addons enable
+minikube addons enable metrics-server
 minikube addons enable ingress
-
-# Abrir o dashboard do Minikube
-echo "Abrindo o dashboard do Minikube..."
-minikube dashboard &
 
 # Rodar a aplicação com Helm
 echo "Rodando a aplicação com Helm..."
 cd calculadora-chart
 helm install calculadora .
 
+# Abrir o dashboard do Minikube
+echo "Abrindo o dashboard do Minikube..."
+minikube dashboard &
 
